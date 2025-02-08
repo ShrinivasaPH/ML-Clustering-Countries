a678f1236fbbb6c1473d174773b9d53249813ca4
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2025-02-08 16:10:10 +0530

    Update display_dataset.py

--- display_dataset.py
+++ display_dataset.py
@@ -2,7 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('Country-data.csv')
-st.title(":blue[Clustering Economies of Countries using Machine Learning]")
-st.dataframe(df.head())
-st.caption("Sample view of the dataset.")
+
+def display_dataset():
+    st.title("Country Clusters")
+    df = pd.read_csv('Country-data.csv')
+    st.title(":blue[Clustering Economies of Countries using Machine Learning]")
+    st.dataframe(df.head())
+    st.caption("Sample view of the dataset.")
+
+pg = st.navigation([st.Page("display_dataset.py"), st.Page(display_dataset)])
+pg.run()
